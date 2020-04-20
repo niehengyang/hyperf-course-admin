1a29e4ae67326f1fe84fac6d73968a472f9b6073
niehengyang <niehengyang@ebupt.com>
2020-04-20 18:21:23 +0800

    增加首页

--- src/home/home.js
+++ src/home/home.js
@@ -3,20 +3,6 @@ import Vue from 'vue'
 /** 第三方通用组件 **/
 require('../bootstrap/3rd');
 
-require('../../src/assets/js/home/bootstrap.min');
-require('../../src/assets/js/home/classie');
-require('../../src/assets/js/home/html5shiv.min');
-require('../../src/assets/js/home/jquery.appear');
-require('../../src/assets/js/home/jquery.magnific-popup.min');
-require('../../src/assets/js/home/jquery-1.9.1.min');
-require('../../src/assets/js/home/main');
-require('../../src/assets/js/home/masonry');
-require('../../src/assets/js/home/masonry.pkgd.min');
-require('../../src/assets/js/home/owl.carousel.min');
-require('../../src/assets/js/home/smooth-scroll.min');
-require('../../src/assets/js/home/typed');
-
-
 import Home from './Home.vue'
 import baseVueRouter from "vue-router"; // 引入App最外层页面组件
 
